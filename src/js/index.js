38a3ea2d430685aa3e3bdbd3af871a6c76cb3c7d
Stanislau Vyrski <vyr4376@yandex.ru>
2024-01-17 14:12:23 +0300

    feat: add button. add sounds effects (Wed, Jan 17, 2024, 2:12:14 PM)

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,10 @@
 const devil = document.querySelector('.devil');
 const playButton = document.querySelector('.play');
-let width = 100;
+let width = 110;
+
+function delay(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 function getCenterOfWidth() {
 	return (document.documentElement.getBoundingClientRect().width / 2);
@@ -14,16 +18,78 @@ function getHeightOfDevil() {
 	return (devil.getBoundingClientRect().height);
 }
 
-function loadPageSound() {
-	new Audio("start-music.mp3").play()
+function clickedDevil() {
+	new Audio("devil-breath.mp3").play()
 }
 
+function callDevil() {
+	new Audio("call-devil.mp3").play()
+}
+
+function playScareSoundOne() {
+	new Audio("scare-sound-one.mp3").play()
+}
+
+function playScareSoundTwo() {
+	new Audio("scare-sound-two.mp3").play()
+}
+
+function playScareSoundThree() {
+	new Audio("scare-sound-three.mp3").play()
+}
+
+function playLaughSound() {
+	new Audio('demonic-laugh.mp3').play()
+}
+
+function playTimerSound() {
+	new Audio('timer-sound.mp3').play()
+}
+
+function dieSound() {
+	new Audio('die-sound.mp3').play()
+}
+
+function finalSound() {
+	new Audio('final-laugh-sound.mp3').play()
+}
+
+devil.addEventListener('click', clickedDevil)
+
 playButton.addEventListener('click', function () {
-	setInterval(() => {
-		width += 0.5
-		devil.style.width = width + 'px'
-		devil.style.left = getCenterOfWidth() - width / 2 + 'px'
-		devil.style.bottom = getCenterOfHeight() - getHeightOfDevil() / 2 + 'px'
-	}, 200);
+	this.classList.add('play__active')
+	callDevil()
+	delay(9000).then(() => {
+		devil.classList.add('devil_appear')
+		playScareSoundOne()
+	})
+	delay(9100).then(() => {
+		setInterval(() => {
+			width += 1
+			devil.style.width = width + 'px'
+			devil.style.left = getCenterOfWidth() - width / 2 + 'px'
+			devil.style.bottom = getCenterOfHeight() - getHeightOfDevil() / 2 + 'px'
+		}, 200);
+	})
+	delay(14000).then(() => {
+		playScareSoundTwo()
+		playLaughSound()
+	})
+	delay(20000).then(() => {
+		playLaughSound()
+	})
+
+	delay(26000).then(() => {
+		playTimerSound()
+		playScareSoundThree()
+	})
+
+	delay(37000).then(() => {
+		finalSound()
+	})
+
+	delay(45000).then(() => {
+		dieSound()
+	})
 
 })
