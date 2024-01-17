a4015111416d7480d9e713b492c9686c471b1e1b
Stanislau Vyrski <vyr4376@yandex.ru>
2024-01-17 19:19:34 +0300

    feat: change sounds.add book-image.add shake effect on book (Wed, Jan 17, 2024, 7:19:24 PM)

--- src/js/index.js
+++ src/js/index.js
@@ -1,95 +1,101 @@
 const devil = document.querySelector('.devil');
 const playButton = document.querySelector('.play');
+const deathBackground = document.querySelector('.death-display');
+const newGameButton = document.querySelector('.death-display__button');
+const book = document.querySelector('.book')
+const bookStar = document.querySelector('.book__star')
+const bookEllipse = document.querySelector('.book__ellipse')
 let width = 110;
 
 function delay(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function getCenterOfWidth() {
-	return (document.documentElement.getBoundingClientRect().width / 2);
-}
-
-function getCenterOfHeight() {
-	return (document.documentElement.getBoundingClientRect().height / 2);
+function getCenterOfDisplay(value) {
+	return (document.documentElement.getBoundingClientRect()[value] / 2);
 }
 
 function getHeightOfDevil() {
 	return (devil.getBoundingClientRect().height);
 }
 
-function clickedDevil() {
+function clickedDevilSound() {
 	new Audio("devil-breath.mp3").play()
 }
 
-function callDevil() {
+function startGame() {
+	playButton.classList.add('play_active');
+	book.classList.add('book_red')
+	bookStar.classList.add('book__star_red');
+	bookEllipse.classList.add('book__ellipse_red')
 	new Audio("call-devil.mp3").play()
 }
 
-function playScareSoundOne() {
-	new Audio("scare-sound-one.mp3").play()
-}
-
-function playScareSoundTwo() {
-	new Audio("scare-sound-two.mp3").play()
-}
-
-function playScareSoundThree() {
-	new Audio("scare-sound-three.mp3").play()
-}
-
-function playLaughSound() {
-	new Audio('demonic-laugh.mp3').play()
-}
-
-function playTimerSound() {
-	new Audio('timer-sound.mp3').play()
-}
-
-function dieSound() {
-	new Audio('die-sound.mp3').play()
-}
-
-function finalSound() {
-	new Audio('final-laugh-sound.mp3').play()
-}
-
-devil.addEventListener('click', clickedDevil)
-
-playButton.addEventListener('click', function () {
-	this.classList.add('play__active')
-	callDevil()
-	delay(9000).then(() => {
-		devil.classList.add('devil_appear')
-		playScareSoundOne()
+function showDevil() {
+	delay(8000).then(() => {
+		book.classList.remove('book_red')
+		book.classList.add('book_hide')
+		devil.classList.add('devil_show')
+		devil.style.left = getCenterOfDisplay('width') - width / 2 + 'px'
+		devil.style.bottom = getCenterOfDisplay('height') - getHeightOfDevil() / 2 + 'px'
+		new Audio("main-sound.mp3").play()
 	})
-	delay(9100).then(() => {
+}
+
+function scaleDevil() {
+	delay(9001).then(() => {
 		setInterval(() => {
-			width += 1
+			width += 1.8
 			devil.style.width = width + 'px'
-			devil.style.left = getCenterOfWidth() - width / 2 + 'px'
-			devil.style.bottom = getCenterOfHeight() - getHeightOfDevil() / 2 + 'px'
+			devil.style.left = getCenterOfDisplay('width') - width / 2 + 'px'
+			devil.style.bottom = getCenterOfDisplay('height') - getHeightOfDevil() / 2 + 'px'
 		}, 200);
 	})
-	delay(14000).then(() => {
-		playScareSoundTwo()
-		playLaughSound()
+}
+
+function playScaredSounds() {
+	delay(12000).then(() => {
+		new Audio("laugh-sound.mp3").play()
 	})
-	delay(20000).then(() => {
-		playLaughSound()
+	delay(15000).then(() => {
+		new Audio("scare-sound-one.mp3").play()
+	})
+	delay(21000).then(() => {
+		new Audio("scare-sound-two.mp3").play()
+	})
+	delay(35000).then(() => {
+		new Audio("scare-sound-four.mp3").play()
+	})
+	delay(42000).then(() => {
+		new Audio("scare-sound-three.mp3").play()
+		new Audio("timer-sound.mp3").play()
 	})
 
-	delay(26000).then(() => {
-		playTimerSound()
-		playScareSoundThree()
+	delay(54000).then(() => {
+		new Audio("die-sound.mp3").play()
 	})
+}
 
-	delay(37000).then(() => {
-		finalSound()
+function showDeathDisplay() {
+	delay(57000).then(() => {
+		new Audio("final-laugh-sound.mp3").play()
+		deathBackground.classList.add('death-display_show')
+		devil.classList.remove('devil_show')
 	})
+}
 
-	delay(45000).then(() => {
-		dieSound()
-	})
+function loadGame() {
+	startGame()
 
-})
+	showDevil()
+
+	scaleDevil()
+
+	playScaredSounds()
+
+	showDeathDisplay()
+}
+
+devil.addEventListener('click', clickedDevilSound)
+
+playButton.addEventListener('click', loadGame)
