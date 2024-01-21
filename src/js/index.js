cdcb5c42c6db1f31ad9df1efe9889e030176644c
Stanislau Vyrski <vyr4376@yandex.ru>
2024-01-21 17:47:27 +0300

    feat: reduced the number of audio files. added a victory screen and a defeat screen (Sun, Jan 21, 2024, 5:47:08 PM)

--- src/js/index.js
+++ src/js/index.js
@@ -1,44 +1,39 @@
 const devil = document.querySelector('.devil');
 const playButton = document.querySelector('.play');
 const deathBackground = document.querySelector('.death-display');
+const deathText = document.querySelector('.death-display__text')
 const newGameButton = document.querySelector('.death-display__button');
 const book = document.querySelector('.book');
 const bookStar = document.querySelector('.book__star');
 const bookEllipse = document.querySelector('.book__ellipse');
 const questionsBlock = document.querySelector('.questions');
-const pento = document.querySelector('.pento')
-
+const searchedItem = document.querySelector('.pento');
+const pento = document.querySelectorAll('.questions__pento');
 
 let width = 110;
 
+function checkWidthIncreaseFactor() {
+	const height = getSizeOfDisplay('height');
+	const width = getSizeOfDisplay('width');
 
-function ret() {
-	if (document.documentElement.getBoundingClientRect().width <= 560) {
-		return 0.8
-	}
-	if (document.documentElement.getBoundingClientRect().height >= 1000) {
-		return 3.2
-	}
-	else if (document.documentElement.getBoundingClientRect().height < 1000 && document.documentElement.getBoundingClientRect().height >= 850) {
-		return 2.7
-	}
-	else if (document.documentElement.getBoundingClientRect().height < 850 && document.documentElement.getBoundingClientRect().height >= 780) {
-		return 2.4
-	}
-	else if (document.documentElement.getBoundingClientRect().height < 780 && document.documentElement.getBoundingClientRect().height >= 702) {
-		return 2.1
-	}
-	else if (document.documentElement.getBoundingClientRect().height < 702 && document.documentElement.getBoundingClientRect().height >= 590) {
-		return 1.8
-	}
-	else if (document.documentElement.getBoundingClientRect().height < 590 && document.documentElement.getBoundingClientRect().height >= 525) {
-		return 1.5
-	}
-	else if (document.documentElement.getBoundingClientRect().height < 525 && document.documentElement.getBoundingClientRect().height >= 450) {
-		return 1.2
-	}
-	else if (document.documentElement.getBoundingClientRect().height < 450) {
-		return 0.8
+	if (width <= 560) {
+		return 0.7
+	} if (height >= 1000) {
+		return 3.1
+	} else if (height < 1000 && height >= 850) {
+		return 2.6
+	} else if (height < 850 && height >= 780) {
+		return 2.3
+	} else if (height < 780 && height >= 702) {
+		return 2
+	} else if (height < 702 && height >= 590) {
+		return 1.7
+	} else if (height < 590 && height >= 525) {
+		return 1.4
+	} else if (height < 525 && height >= 450) {
+		return 1.1
+	} else if (height < 450) {
+		return 0.7
 	}
 }
 
@@ -46,17 +41,25 @@ function delay(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function getSizeOfDisplay(value) {
+	return (document.documentElement.getBoundingClientRect()[value]);
+}
+
 function getCenterOfDisplay(value) {
-	return (document.documentElement.getBoundingClientRect()[value] / 2);
+	return getSizeOfDisplay(value) / 2
 }
 
 function getHeightOfDevil() {
 	return (devil.getBoundingClientRect().width);
 }
 
+function getSizeOfBook(value) {
+	return book.getBoundingClientRect()[value];
+}
+
 function positioningBook() {
-	book.style.left = getCenterOfDisplay('width') - book.getBoundingClientRect().width / 2 + 'px'
-	book.style.bottom = getCenterOfDisplay('height') - book.getBoundingClientRect().height / 2 + 'px'
+	book.style.left = getCenterOfDisplay('width') - getSizeOfBook('width') / 2 + 'px'
+	book.style.bottom = getCenterOfDisplay('height') - getSizeOfBook('height') / 2 + 'px'
 }
 positioningBook()
 
@@ -74,7 +77,6 @@ function clickedDevilSound() {
 }
 
 function startGame() {
-
 	playButton.classList.add('play_active');
 	book.classList.add('book_red')
 	bookStar.classList.add('book__star_red');
@@ -83,42 +85,61 @@ function startGame() {
 	new Audio("shake-book.mp3").play()
 }
 
-function generateRandomPositionOfPentoLeft() {
+function generateSearchedItemNumLeft() {
 	let min = 25;
 	let max = Math.floor(getCenterOfDisplay('width') - 25);
 	return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function generateRandomPositionOfPentoBottom() {
+function generateSearchedItemNumBottom() {
 	let min = 25;
 	let max = Math.floor(getCenterOfDisplay('height') - 25);
 	return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+const mainThemeSound = new Audio("main-sound.mp3")
+
 function showDevil() {
 	delay(8000).then(() => {
-		pento.style.display = 'inline-block';
-		pento.style.left = generateRandomPositionOfPentoLeft() * 2
-		pento.style.bottom = generateRandomPositionOfPentoBottom() * 2
+		searchedItem.style.display = 'inline-block';
+		searchedItem.style.left = generateSearchedItemNumLeft() * 2
+		searchedItem.style.bottom = generateSearchedItemNumBottom() * 2
 		book.classList.remove('book_red')
 		book.classList.add('book_hide')
 		devil.classList.add('devil_show')
 		devil.style.left = getCenterOfDisplay('width') - width / 2 + 'px'
 		devil.style.bottom = getCenterOfDisplay('height') - getHeightOfDevil() / 2 + 'px'
-		new Audio("main-sound.mp3").play()
+		mainThemeSound.play()
 	})
 }
 
-pento.addEventListener('click', function (e) {
-	pento.style.left = generateRandomPositionOfPentoLeft() * 2
-	pento.style.bottom = generateRandomPositionOfPentoBottom() * 2
+let count = 0;
+
+searchedItem.addEventListener('click', function (e) {
+
+	if (count < 10) {
+		if (count === 5) {
+			new Audio('50-completed-sound.mp3').play()
+		}
+		new Audio('finded-sound.mp3').play()
+		searchedItem.style.left = generateSearchedItemNumLeft() * 2
+		searchedItem.style.bottom = generateSearchedItemNumBottom() * 2
+
+		pento[count].classList.add('pento_finded')
+		++count
+	}
+	if (count === 10) {
+		showResultDisplay('YOU ALIVE', 'death-display_win')
+		mainThemeSound.pause()
+	}
 })
+
+let timerId;
 
 function scaleDevil() {
 	delay(9001).then(() => {
-		setInterval(() => {
-			let scaleWidth = ret()
-			console.log(scaleWidth);
+		timerId = setInterval(() => {
+			let scaleWidth = checkWidthIncreaseFactor()
 			width += scaleWidth
 			devil.style.width = width + 'px'
 			devil.style.left = getCenterOfDisplay('width') - width / 2 + 'px'
@@ -127,35 +148,11 @@ function scaleDevil() {
 	})
 }
 
-function playScaredSounds() {
-	delay(12000).then(() => {
-		new Audio("laugh-sound.mp3").play()
-	})
-	delay(15000).then(() => {
-		new Audio("scare-sound-one.mp3").play()
-	})
-	delay(21000).then(() => {
-		new Audio("scare-sound-two.mp3").play()
-	})
-	delay(35000).then(() => {
-		new Audio("scare-sound-four.mp3").play()
-	})
-	delay(42000).then(() => {
-		new Audio("scare-sound-three.mp3").play()
-		new Audio("timer-sound.mp3").play()
-	})
-
-	delay(54000).then(() => {
-		new Audio("die-sound.mp3").play()
-	})
-}
-
-function showDeathDisplay() {
-	delay(57000).then(() => {
-		new Audio("final-laugh-sound.mp3").play()
-		deathBackground.classList.add('death-display_show')
-		devil.classList.remove('devil_show')
-	})
+function showResultDisplay(value, classList) {
+	deathBackground.classList.add(classList)
+	devil.classList.remove('devil_show')
+	deathText.textContent = value
+	clearInterval(timerId)
 }
 
 function loadGame() {
@@ -164,12 +161,21 @@ function loadGame() {
 	showDevil()
 
 	scaleDevil()
-
-	playScaredSounds()
-
-	showDeathDisplay()
 }
 
 devil.addEventListener('click', clickedDevilSound)
 
 playButton.addEventListener('click', loadGame)
+
+let timer = 0;
+
+setInterval(() => {
+	++timer
+	if (timer === 58) {
+		showResultDisplay('YOU DIED', 'death-display_loose')
+	}
+}, 1000)
+
+newGameButton.addEventListener('click', function () {
+	location.reload()
+})
