a23d9e0e9de72cb04af8602cf8b6d335087a9dd3
Stanislau <vyr4376@yandex.ru>
2024-06-25 17:29:13 +0300

    refactor: the devil variable has been moved to the centeringstartdevil module (Tue, Jun 25, 2024, 5:29:09 PM)

--- src/js/centeringStartDevil.js
+++ src/js/centeringStartDevil.js
@@ -1,6 +1,8 @@
 import { getCenterOfDisplay, getSizeOfDevil } from ".."
 import { devil } from "./showDevil"
 
+export const devil = document.querySelector('.devil')
+
 export const centeringStartDevil = () => {
 	devil.style.left = getCenterOfDisplay('width') - getSizeOfDevil('width') / 2 + 'px'
 	devil.style.bottom = getCenterOfDisplay('height') - getSizeOfDevil('height') / 2 + 'px'
