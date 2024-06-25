75563557cf10500291d5523901ae87ee1f32e353
Stanislau <vyr4376@yandex.ru>
2024-06-25 17:18:19 +0300

    feat: the centeringstartdevil function is used in the showdevil function (Tue, Jun 25, 2024, 5:18:15 PM)

--- src/js/showDevil.js
+++ src/js/showDevil.js
@@ -1,4 +1,5 @@
-import { delay, book, getSizeOfDevil, getCenterOfDisplay, generateSearchedItemNum, getSpeedOfSearchItem } from "..";
+import { delay, book, getSizeOfDevil, generateSearchedItemNum, getSpeedOfSearchItem } from "..";
+import { centeringStartDevil } from "./centeringStartDevil";
 import { controlGameSounds } from "./controlGameSounds";
 import { searchedItemSize, setSearchedItemSize } from "./setSearchedItemSize";
 import { setStartWidthOfDevil } from "./setStartWidthOfDevil";
@@ -8,14 +9,14 @@ export const searchedItem = document.querySelector('.searched-item')
 
 export function showDevil() {
 	delay(8000).then(() => {
+		const widthOfDevil = getSizeOfDevil('width')
 		searchedItem.style.display = 'inline-block';
 		setSearchedItemSize()
 		book.classList.remove('book_shake')
 		book.classList.add('book_hide')
 		devil.classList.add('devil_show')
-		setStartWidthOfDevil(getSizeOfDevil('width'))
-		devil.style.left = getCenterOfDisplay('width') - getSizeOfDevil('width') / 2 + 'px'
-		devil.style.bottom = getCenterOfDisplay('height') - getSizeOfDevil('height') / 2 + 'px'
+		setStartWidthOfDevil(widthOfDevil)
+		centeringStartDevil()
 		controlGameSounds('mainSound', 'play')
 		setInterval(() => {
 			searchedItem.style.left = generateSearchedItemNum(searchedItemSize, 'width') + 'px'
