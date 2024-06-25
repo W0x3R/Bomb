1e645a0996432868f5ed6797ce1261b57832c084
Stanislau <vyr4376@yandex.ru>
2024-06-25 17:28:22 +0300

    feat: the addshowdevilstyles module and function have been created. (Tue, Jun 25, 2024, 5:28:06 PM)

---
+++ src/js/addShowDevilStyles.js
@@ -0,0 +1,9 @@
+import { book } from "..";
+import { devil, searchedItem } from "./showDevil";
+
+export const addShowDevilStyles = () => {
+	searchedItem.style.display = 'inline-block';
+	book.classList.remove('book_shake')
+	book.classList.add('book_hide')
+	devil.classList.add('devil_show')
+}
