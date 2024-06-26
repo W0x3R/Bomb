fa4b2a5fa00a2c09156dbb42f12475cacd6da8d0
Stanislau <vyr4376@yandex.ru>
2024-06-26 16:11:03 +0300

    feat: the speedofsearchitem variable has been moved from the setspeedofsearchitem function to the new speedofsearchitem module (Wed, Jun 26, 2024, 4:10:59 PM)

---
+++ src/js/speedOfSearchItem.js
@@ -0,0 +1,8 @@
+export const speedOfSearchItem = [
+	{ width: 1400, speed: 790 },
+	{ width: 1200, speed: 810 },
+	{ width: 1100, speed: 790 },
+	{ width: 1000, speed: 770 },
+	{ width: 850, speed: 760 },
+	{ width: 320, speed: 650 }
+]
