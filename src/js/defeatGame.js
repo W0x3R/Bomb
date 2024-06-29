9c65815d227f331e96959c24c9c1eecefa3e4602
Stanislau <vyr4376@yandex.ru>
2024-06-29 19:29:45 +0300

    feat: created a new module with defeatgame function to control the application during defeat. (Sat, Jun 29, 2024, 7:29:41 PM)

---
+++ src/js/defeatGame.js
@@ -0,0 +1,17 @@
+import { showResultDisplay } from ".."
+import { delay } from "./delay"
+import { searchedItem } from "./showDevil"
+import { controlGameSounds } from "./sounds/controlGameSounds"
+import { clearTimerCheckEndGame } from "./timerCheckEndGame"
+
+export const defeatGame = () => {
+	showResultDisplay('YOU DIED', 'result-display_defeat', 'result-display__button_defeat')
+	searchedItem.remove()
+	delay(8000).then(() => {
+		controlGameSounds('defeat', 'play')
+	})
+	delay(26000).then(() => {
+		controlGameSounds('defeatSecond', 'play')
+	})
+	clearTimerCheckEndGame()
+}
