e90f4a5bb399d7c6c137750ef070818f6d877f70
Stanislau <vyr4376@yandex.ru>
2024-06-29 19:31:43 +0300

    feat: the functionality that handled events when clicking on a searcheditem has been moved to the new module in the form of the function clickonsearcheditem (Sat, Jun 29, 2024, 7:31:39 PM)

--- src/index.js
+++ src/index.js
@@ -4,26 +4,22 @@ import { searchedItem, showDevil } from './js/showDevil.js';
 import { setStartWidthOfDevil, startWidthOfDevil } from './js/setStartWidthOfDevil.js';
 import { centeringItems, devil } from './js/centeringItems.js';
 import { getSizeOfItem } from './js/getSizeOfItem.js';
-import { controlGameSounds } from './js/sounds/controlGameSounds.js';
 import { getScaleCoefficientOfDevil } from './js/getScaleCoefficientOfDevil.js';
 import { clickEvents } from './js/eventHandlers/clickEvents.js';
 import { callEvents } from './js/eventHandlers/callEvents.js';
 import { setSearchedItemSize } from './js/searchedItem/setSearchedItemSize.js';
 import { setRulesLanguage } from './js/setRulesLanguage.js';
 import { delay } from './js/delay.js';
+import { startTimerCheckEndGame } from './js/timerCheckEndGame.js';
+import { clickOnSearchedItem } from './js/searchedItem/clickOnSearchedItem.js';
+import { defeatGame } from './js/defeatGame.js';
 
 const resultDisplay = document.querySelector('.result-display')
 const resultText = document.querySelector('.result-display__text')
 const newGameButton = document.querySelector('.result-display__button')
 export const book = document.querySelector('.book')
-
-const foundItemsBlock = document.querySelector('.found-items')
-
-const pento = document.querySelectorAll('.found-items__pento')
-
-let count = 0
 let timer = 0
-let timerCheckEndGame
+
 let timerScaleDevil
 
 window.addEventListener('click', (e) => callEvents(e, clickEvents))
@@ -39,26 +35,8 @@ export function getCenterOfDisplay(value) {
 	return getSizeOfItem(document.documentElement, value) / 2
 }
 
-searchedItem.addEventListener('click', function () {
-	if (count < 10) {
+searchedItem.addEventListener('click', clickOnSearchedItem)
 
-		if (count === 4) {
-			foundItemsBlock.classList.add('found-items__pento_shake')
-		}
-		controlGameSounds('searchedItem', 'play')
-		pento[count].classList.add('found-items__pento_found')
-		++count
-
-	} if (count === 10) {
-		controlGameSounds('mainSound', 'pause')
-		this.remove()
-		delay(1000).then(() => {
-			showResultDisplay('YOU SURVIVED', 'result-display_win', 'result-display__button_win')
-			controlGameSounds('win', 'play')
-		})
-		clearInterval(timerCheckEndGame)
-	}
-})
 function scaleDevil() {
 	delay(9001).then(() => {
 		timerScaleDevil = setInterval(() => {
@@ -71,7 +49,7 @@ function scaleDevil() {
 	})
 }
 
-function showResultDisplay(value, classList, newGameStyle) {
+export function showResultDisplay(value, classList, newGameStyle) {
 	resultDisplay.classList.add(classList)
 	resultText.textContent = value
 	clearInterval(timerScaleDevil)
@@ -83,23 +61,12 @@ export function loadGame() {
 	showDevil()
 	scaleDevil()
 
-	timerCheckEndGame = setInterval(checkEndOfGame, 1000)
+	startTimerCheckEndGame()
 	startGameButton.setAttribute('disabled', true)
 }
 
-function checkEndOfGame() {
-	++timer
-	if (timer === 56) {
-		showResultDisplay('YOU DIED', 'result-display_defeat', 'result-display__button_defeat')
-		searchedItem.remove()
-		delay(8000).then(() => {
-			controlGameSounds('defeat', 'play')
-		})
-		delay(26000).then(() => {
-			controlGameSounds('defeatSecond', 'play')
-		})
-		clearInterval(timerCheckEndGame);
-	}
+export function checkEndOfGame() {
+	timer < 56 ? ++timer : defeatGame()
 }
 
 setRulesLanguage()
---
+++ src/js/searchedItem/clickOnSearchedItem.js
@@ -0,0 +1,18 @@
+import { controlGameSounds } from "../sounds/controlGameSounds"
+import { winGame } from "../winGame"
+
+const foundItemsBlock = document.querySelector('.found-items')
+const pento = document.querySelectorAll('.found-items__pento')
+let count = 0
+
+export const clickOnSearchedItem = () => {
+	controlGameSounds('searchedItem', 'play')
+	pento[count].classList.add('found-items__pento_found')
+	++count
+	if (count === 5) {
+		foundItemsBlock.classList.add('found-items__pento_shake')
+	}
+	if (count === 10) {
+		winGame()
+	}
+}
