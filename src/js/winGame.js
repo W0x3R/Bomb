1bec278b0925e8579a7de2296c155afff91b3711
Stanislau <vyr4376@yandex.ru>
2024-06-29 19:29:05 +0300

    feat: created a new module with wingame function to control the application during win. (Sat, Jun 29, 2024, 7:29:02 PM)

---
+++ src/js/winGame.js
@@ -0,0 +1,15 @@
+import { showResultDisplay } from ".."
+import { delay } from "./delay"
+import { searchedItem } from "./showDevil"
+import { controlGameSounds } from "./sounds/controlGameSounds"
+import { clearTimerCheckEndGame } from "./timerCheckEndGame"
+
+export const winGame = () => {
+	controlGameSounds('mainSound', 'pause')
+	searchedItem.remove()
+	delay(500).then(() => {
+		showResultDisplay('YOU SURVIVED', 'result-display_win', 'result-display__button_win')
+		controlGameSounds('win', 'play')
+	})
+	clearTimerCheckEndGame()
+}
