529085a9089f2f5fc41f7c45d01ec9158062bd2c
Stanislau Vyrski <vyr4376@yandex.ru>
2024-07-07 20:59:10 +0300

    fix: changed values in the speedofsearchobject object (Sun, Jul 7, 2024, 8:59:06 PM)

--- src/js/searchedObject/speedOfSearchObject.js
+++ src/js/searchedObject/speedOfSearchObject.js
@@ -1,8 +1,5 @@
 export const speedOfSearchObject = [
-	{ width: 1400, speed: 790 },
-	{ width: 1200, speed: 810 },
-	{ width: 1100, speed: 790 },
-	{ width: 1000, speed: 770 },
-	{ width: 850, speed: 760 },
+	{ width: 1000, speed: 760 },
+	{ width: 850, speed: 725 },
 	{ width: 320, speed: 650 }
 ]
