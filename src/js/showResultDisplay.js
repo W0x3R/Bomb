c515f81eb0f9507e8bd8366560c1fb162d73a86a
Stanislau Vyrski <vyr4376@yandex.ru>
2024-07-07 22:04:09 +0300

    feat: added gamecontainer variable in the showresultdisplay module (Sun, Jul 7, 2024, 10:04:06 PM)

--- src/js/showResultDisplay.js
+++ src/js/showResultDisplay.js
@@ -1,14 +1,14 @@
-import { controlDisplayDevil } from "./devil/controlDisplayDevil"
 import { timerScaleDevil } from "./devil/scaleDevil"
 
 const resultDisplay = document.querySelector('.result-display')
 const resultText = document.querySelector('.result-display__text')
 const newGameButton = document.querySelector('.result-display__button')
+const gameContainer = document.querySelector('.container')
 
 export const showResultDisplay = (value, classList, newGameStyle) => {
 	resultDisplay.classList.add(classList)
 	resultText.textContent = value
 	clearInterval(timerScaleDevil)
 	newGameButton.classList.add(newGameStyle)
-	controlDisplayDevil('remove')
+	gameContainer.remove()
 }
