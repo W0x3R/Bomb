3e00e0841d42d6bbbb5fa6b15fc93a8ef2c23ad1
Stanislau Vyrski <vyr4376@yandex.ru>
2024-07-09 21:44:32 +0300

    fix: removed audio files with audio__change-language and audio__searched-item classes. also removed these audio files from the appsounds file. (Tue, Jul 9, 2024, 9:45:45 PM)

--- src/js/sounds/appSounds.js
+++ src/js/sounds/appSounds.js
@@ -1,8 +1,6 @@
 export const appSounds = {
 	mainSound: document.querySelector('.auido__main'),
 	callDevil: document.querySelector('.auido__call-devil'),
-	changeLanguage: document.querySelector('.audio__change-language'),
-	searchedItem: document.querySelector('.audio__searched-item'),
 	shakeBook: document.querySelector('.audio__shake-book'),
 	win: document.querySelector('.audio__win'),
 	defeat: document.querySelector('.audio__defeat'),
