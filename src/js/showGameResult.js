1ad49adf74ebde534afe6224d40e8c14498cae25
Stanislau Vyrski <vyr4376@yandex.ru>
2024-07-11 12:12:46 +0300

    fix: the devilpictureblock variable was created.instead of deleting gamecontainer.remove(), the deletion of individual elements: devilpictureblock and foundobjectsblock is used now (Thu, Jul 11, 2024, 12:12:38 PM)

--- src/js/showGameResult.js
+++ src/js/showGameResult.js
@@ -1,14 +1,16 @@
 import { timerScaleDevil } from "./devil/scaleDevil"
+import { foundObjectsBlock } from "./searchedObject/clickOnSearchObject"
 
 const resultDisplay = document.querySelector('.result')
 const resultText = document.querySelector('.result__text')
 const newGameButton = document.querySelector('.result__button')
-const gameContainer = document.querySelector('.container')
+const devilPictureBlock = document.querySelector('.devil__picture')
 
 export const showGameResult = (value, classList, newGameStyle) => {
 	resultDisplay.classList.add(classList)
 	resultText.textContent = value
 	clearInterval(timerScaleDevil)
 	newGameButton.classList.add(newGameStyle)
-	gameContainer.remove()
+	devilPictureBlock.remove()
+	foundObjectsBlock.remove()
 }
