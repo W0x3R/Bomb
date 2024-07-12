2bb33013568c45becc5177aaa2ecb47309f2cc5b
Stanislau Vyrski <vyr4376@yandex.ru>
2024-07-12 17:33:00 +0300

    feat: the movingsearchobject function is used inside the function (Fri, Jul 12, 2024, 5:32:52 PM)

--- src/js/searchedObject/clickOnSearchObject.js
+++ src/js/searchedObject/clickOnSearchObject.js
@@ -1,6 +1,7 @@
 import foundSearchedItem from '../../sounds/foundSearchedItemSound.mp3'
 import { winGame } from "../gameResult/winGame"
 import { constructSound } from '../sounds/constructSound'
+import { movingSearchObject } from './movingSearchObject'
 
 export const foundObjectsBlock = document.querySelector('.found-items')
 const pento = document.querySelectorAll('.found-items__pento')
@@ -8,6 +9,7 @@ let count = 0
 
 export const clickOnSearchObject = () => {
 	constructSound(foundSearchedItem)
+	movingSearchObject()
 	pento[count].classList.add('found-items__pento_found')
 	++count
 	if (count === 5) {
