f0048007a45c90639d01fc6e46f576e6fc01e6b8
Stanislau Vyrski <vyr4376@yandex.ru>
2024-07-12 17:25:26 +0300

    fix: in the startgame function, the calls of the setsearchobjectsize and movingsearchobject functions have been removed. in the function showdevil the calls of functions setsearchobjectsize and movingsearchobject were added. (Fri, Jul 12, 2024, 5:26:41 PM)

--- src/js/devil/showDevil.js
+++ src/js/devil/showDevil.js
@@ -2,19 +2,22 @@ import { addShowDevilStyles } from "./addShowDevilStyles";
 import { centeringElement, devil } from "../sizesAndcentering/centeringElement";
 import { delay } from "../delay";
 import { getSizeOfElement } from "../sizesAndcentering/getSizeOfElement";
-import { movingSearchObjectWithInterval } from "../searchedObject/movingSearchObject";
+import { movingSearchObject, movingSearchObjectWithInterval } from "../searchedObject/movingSearchObject";
 import { setStartWidthOfDevil } from "./setStartWidthOfDevil";
 import { controlGameSounds } from "../sounds/controlGameSounds";
 import { displayFoundItemsBlock } from '../displayFoundItemsBlock'
+import { setSearchObjectSize } from "../searchedObject/setSearchObjectSize";
 
 export const showDevil = () => {
 	delay(8000).then(() => {
 		addShowDevilStyles()
+		setSearchObjectSize()
+		movingSearchObject()
 		const widthOfDevil = getSizeOfElement(devil, 'width')
 		setStartWidthOfDevil(widthOfDevil)
 		centeringElement(devil)
 		controlGameSounds('mainSound', 'play')
-		movingSearchObjectWithInterval()
 		displayFoundItemsBlock('add')
+		movingSearchObjectWithInterval()
 	})
 }
--- src/js/startGame/startGame.js
+++ src/js/startGame/startGame.js
@@ -2,13 +2,9 @@ import { addStartingStyles, startGameButton } from "./addStartingStyles"
 import { scaleDevil } from "../devil/scaleDevil"
 import { showDevil } from "../devil/showDevil"
 import { startTimerCheckEndGame } from "../checkEndGame/startTimer"
-import { movingSearchObject } from "../searchedObject/movingSearchObject"
-import { setSearchObjectSize } from "../searchedObject/setSearchObjectSize"
 
 export const startGame = () => {
-	setSearchObjectSize()
 	addStartingStyles()
-	movingSearchObject()
 	showDevil()
 	scaleDevil()
 	startTimerCheckEndGame()
