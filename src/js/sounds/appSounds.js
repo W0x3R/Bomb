930cde7f0dec8fc801df2cffea7e06c2e356c83b
Stanislau Vyrski <vyr4376@yandex.ru>
2024-07-12 15:10:48 +0300

    refactor: removed audio files with audio__win, audio__defeat, and audio__defeat-second classes. (Fri, Jul 12, 2024, 3:10:44 PM)

--- src/js/sounds/appSounds.js
+++ src/js/sounds/appSounds.js
@@ -2,7 +2,4 @@ export const appSounds = {
 	mainSound: document.querySelector('.auido__main'),
 	callDevil: document.querySelector('.auido__call-devil'),
 	shakeBook: document.querySelector('.audio__shake-book'),
-	win: document.querySelector('.audio__win'),
-	defeat: document.querySelector('.audio__defeat'),
-	defeatSecond: document.querySelector('.audio__defeat-second'),
 }
