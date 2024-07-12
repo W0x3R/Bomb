7b2f08427afafff0cdbe286691fad243f75176c0
Stanislau Vyrski <vyr4376@yandex.ru>
2024-07-12 17:01:53 +0300

    feat: the calls of setsearchobjectsize and movingsearchobject functions have been removed from the showdevil function. the movingsearchobjectwithinterval function call was added. (Fri, Jul 12, 2024, 5:01:30 PM)

--- src/js/devil/showDevil.js
+++ src/js/devil/showDevil.js
@@ -2,8 +2,7 @@ import { addShowDevilStyles } from "./addShowDevilStyles";
 import { centeringElement, devil } from "../sizesAndcentering/centeringElement";
 import { delay } from "../delay";
 import { getSizeOfElement } from "../sizesAndcentering/getSizeOfElement";
-import { movingSearchObject } from "../searchedObject/movingSearchObject";
-import { setSearchObjectSize } from "../searchedObject/setSearchObjectSize";
+import { movingSearchObjectWithInterval } from "../searchedObject/movingSearchObject";
 import { setStartWidthOfDevil } from "./setStartWidthOfDevil";
 import { controlGameSounds } from "../sounds/controlGameSounds";
 import { displayFoundItemsBlock } from '../displayFoundItemsBlock'
@@ -11,12 +10,11 @@ import { displayFoundItemsBlock } from '../displayFoundItemsBlock'
 export const showDevil = () => {
 	delay(8000).then(() => {
 		addShowDevilStyles()
-		setSearchObjectSize()
 		const widthOfDevil = getSizeOfElement(devil, 'width')
 		setStartWidthOfDevil(widthOfDevil)
 		centeringElement(devil)
 		controlGameSounds('mainSound', 'play')
-		movingSearchObject()
+		movingSearchObjectWithInterval()
 		displayFoundItemsBlock('add')
 	})
 }
