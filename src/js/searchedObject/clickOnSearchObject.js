c4cf7282fc341c4d827aa8d1f4af58c99b6f5d51
Stanislau Vyrski <vyr4376@yandex.ru>
2024-07-15 16:52:35 +0300

    fix: removed the movingobject function call inside the clickonsearchobject function. (Mon, Jul 15, 2024, 4:52:32 PM)

--- src/js/searchedObject/clickOnSearchObject.js
+++ src/js/searchedObject/clickOnSearchObject.js
@@ -1,7 +1,6 @@
 import foundSearchedItem from '../../sounds/foundSearchedItemSound.mp3'
 import { winGame } from "../gameResult/winGame"
 import { constructSound } from '../sounds/constructSound'
-import { movingSearchObject } from './movingSearchObject'
 
 export const foundObjectsBlock = document.querySelector('.found-items')
 const pento = document.querySelectorAll('.found-items__pento')
@@ -9,7 +8,6 @@ let count = 0
 
 export const clickOnSearchObject = () => {
 	constructSound(foundSearchedItem)
-	movingSearchObject()
 	pento[count].classList.add('found-items__pento_found')
 	++count
 	if (count === 5) {
